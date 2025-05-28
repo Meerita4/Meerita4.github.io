f31c4b78b2d14761a43f58f02490a79453250490
Alexthfc10 <116588914+meerita4@users.noreply.github.com>
2025-05-28 09:54:54 +0200

    se supone q esta acabado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-// @ts-check
+
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  // Comentamos la configuración para GitHub Pages para desarrollo local
-  // Para producción, descomenta estas líneas:
-  // site: 'https://github.com/Meerita4',
-  // base: '/Portfolio',
+  
 });
+
